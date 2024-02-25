071cb6da2a65f633a82a00022fa64f1d9a418c72
Lake Mallett <spleegness@gmail.com>
2024-02-26 03:20:16 +1100

    removal of lightmode button, yuck

--- .quartz/quartz.layout.ts
+++ .quartz/quartz.layout.ts
@@ -24,7 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.Darkmode(),
+    //Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer({
       filterFn: (node) => node.name !== "Templates",
     })),
